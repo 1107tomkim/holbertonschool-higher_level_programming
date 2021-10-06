4070a8c0a58be88e444b80235d55c58d06bf3251
1107tomkim <cryptotaekim@gmail.com>
2021-10-06 04:53:52 +0000

    PEP8 FORMAT

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"Module is used to create class square"
+
+
 class Square:
+    "Defines class and instantiates"
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
