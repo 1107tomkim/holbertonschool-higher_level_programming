4000e7e879e4a6fb05edd995bf173f03afa52b82
1107tomkim <cryptotaekim@gmail.com>
2021-10-25 19:05:58 +0000

    Writing a class BaseGeometry with certain factors

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines class BaseGeometry"""
+
+
+class BaseGeometry:
+    "Class BaseGeometry"
+
+    def area(self):
+            raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates Values"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be great than 0".format(name))
