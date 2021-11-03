a6fdd3bf804642b95c0e3e61367fd25b02eb3044
1107tomkim <cryptotaekim@gmail.com>
2021-11-03 05:10:04 +0000

    Adding semi-colon at the end of my code

--- 0x00-SQL_introduction/11-best_score.sql
+++ 0x00-SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 --write a script that lists all records with a score >= 10 in the table
 
 SELECT score, name FROM second_table
-WHERE score >= 10 ORDER BY score DESC
+WHERE score >= 10 ORDER BY score DESC;
