6f71727b33ee66c8cba19d7a68c9eae081d9982d
1107tomkim <cryptotaekim@gmail.com>
2021-11-19 04:51:18 +0000

    Takes an arg and display all vals

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Displays all val in staets table where name matches arg"""
+
+from sys import argv
+import MYSQLdb
+
+if __name__ = "__main__":
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    compare = argv[4]
+
+    DB = MySQLdb.connect(host="localhost", user=user,
+                         passwd=password, db=database, port=3306)
+
+    cur = DB.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE name\
+         LIKE '{}' ORDER BY states.id ASC".format(compare))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    DB.close()
