5414d9fbfc1b05333d63c747f8579d4f0806a230
1107tomkim <cryptotaekim@gmail.com>
2021-12-02 02:59:48 +0000

    Changing a val of a data to call upon proper

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     dict = {}
     dict['email'] = argv[2]
     data = parse.urlencode(dict)
-    dict = data.encode('utf-8')
+    data = data.encode('utf-8')
     with request.urlopen(argv[1], data) as response:
         html = response.read()
         print(html.decode('utf-8))
