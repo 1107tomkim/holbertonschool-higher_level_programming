aceb77fdf7f25c0ebd2d37bcf180bd7b23525217
1107tomkim <cryptotaekim@gmail.com>
2021-12-02 04:25:40 +0000

    Fixing indentation error

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         with request.urlopen(argv[1]) as response:
             html = response.read()
             print(html.decode('utf-8'))
-        except error.HTTPError as e:
-            print("Error code: {}".format(e.code))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
