298dc453648054b1b5007888bdd3655c1fd24e5e
1107tomkim <cryptotaekim@gmail.com>
2021-12-02 04:40:56 +0000

    Fetches URL

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetches URL"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+    request = requests.get("https://intranet.hbtn.io/status")
+    print("Body Response:")
+    print("\t- type: {}".format(type(request.text)))
+    print("\t- content: {}".format(request.text))
