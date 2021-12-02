236e10b4729eaa24fa8eab06ccd35408da9388df
1107tomkim <cryptotaekim@gmail.com>
2021-12-02 04:15:36 +0000

    Import fix, and syntax fit for import from

--- 0x0C-python-network_1/1-htbn_header.py
+++ 0x0C-python-network_1/1-htbn_header.py
@@ -2,11 +2,11 @@
 """Sends request to the URL and display val"""
 
 
-import urllib.request
+from urllib import request
 from sys import argv
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(argv[1]) as response:
+    with request.urlopen(argv[1]) as response:
         html = response.info()
     print(html.get("X-Request-Id"))
