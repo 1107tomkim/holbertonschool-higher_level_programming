669f158e51b4eeb95067e09dd115ebdc4f61a9f8
1107tomkim <cryptotaekim@gmail.com>
2021-12-02 05:03:06 +0000

    File name fix

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends request to the URL and display val"""
+
+
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        html = response.info()
+    print(html.get("X-Request-Id"))
